f7e7049cf0624d7d07938a25afe1222ef522f7ab
Harry7986 <emarh.harry.code@gmail.com>
2026-04-06 23:59:50 +0200

    Use Media Quzery 2

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+
+export const useMediaQuery = (query: string): boolean => {
+  const [matches, setMatches] = useState<boolean>(() => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  });
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    
+    // Update state
+    setMatches(mediaQuery.matches);
+
+    // Handler
+    const handler = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
+
+    // Add listener
+    mediaQuery.addEventListener('change', handler);
+
+    // Cleanup
+    return () => {
+      mediaQuery.removeEventListener('change', handler);
+    };
+  }, [query]);
+
+  return matches;
+};
