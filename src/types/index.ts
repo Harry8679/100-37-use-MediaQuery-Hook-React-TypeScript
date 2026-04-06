2f79f013bb4be68c3dd127f7174a4b48b886e958
Harry7986 <emarh.harry.code@gmail.com>
2026-04-06 23:59:02 +0200

    Types 2

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+// Types pour media query - AUCUN ANY
+
+export interface NavItem {
+  id: string;
+  label: string;
+  icon: string;
+  href: string;
+}
+
+export interface LayoutCard {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+}
