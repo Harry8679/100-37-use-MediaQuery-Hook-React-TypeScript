a7650ad97ec7868773b885599434aabe091f8fc0
Harry7986 <emarh.harry.code@gmail.com>
2026-04-08 00:10:19 +0200

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { MediaQueryDemo } from './components/MediaQueryDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <MediaQueryDemo />;
 }
 
-export default App
+export default App;
